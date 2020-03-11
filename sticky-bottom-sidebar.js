1e59d910b482e4f6dc55ec4eb6eb9ffe088d3b2d
Cres Jie <cresjie@gmail.com>
2020-03-12 03:35:54 +0800

    Update sticky-bottom-sidebar.js

--- sticky-bottom-sidebar.js
+++ sticky-bottom-sidebar.js
@@ -1,64 +1,64 @@
 (function($){
-		/**
-		 * right sidebar sticky scrolling
-		 */
-		 
+	/**
+	 * right sidebar sticky scrolling
+	 */
+	 
 
-		 $.fn.stickyBottomSidebar = function(opts){
-		 	if( !opts || !opts.mainContainer ) {
-		 		console.error('mainContainer is required');
-		 		return;
-		 	}
+	$.fn.stickyBottomSidebar = function(opts){
+	 	if( !opts || !opts.mainContainer ) {
+	 		console.error('mainContainer is required');
+	 		return;
+	 	}
+		
+		var $el = this;
+
+		var previousScroll = 0;
+
+		 $(window).on('scroll', function(){
+			var currentScroll = $(window).scrollTop();
 			
-			var $el = this;
-
-			var previousScroll = 0;
-
-			 $(window).on('scroll', function(){
-				var currentScroll = $(window).scrollTop();
-				
-				/**
-				 * sticky sidebar only available if the sidebar in not on full width within the main parent container
-				 */
-				
-				if( $el.parent().outerWidth() != $(opts.mainContainer).outerWidth() ){
-					if( $el.hasClass('sticky-sidebar') ) {
-						var bottom = ( $(document).scrollTop() + $(window).outerHeight() ) - ( $(opts.mainContainer).offset().top + $(opts.mainContainer).outerHeight() ) ;
-						
-						
-						bottom = bottom > 0 ? bottom : 0;
-								
-						$el.css({bottom: bottom});
-					}
-
+			/**
+			 * sticky sidebar only available if the sidebar in not on full width within the main parent container
+			 */
+			
+			if( $el.parent().outerWidth() != $(opts.mainContainer).outerWidth() ){
+				if( $el.hasClass('sticky-sidebar') ) {
+					var bottom = ( $(document).scrollTop() + $(window).outerHeight() ) - ( $(opts.mainContainer).offset().top + $(opts.mainContainer).outerHeight() ) ;
 					
-					if(previousScroll < currentScroll) {
-						if( !$el.hasClass('sticky-sidebar') ) {
+					
+					bottom = bottom > 0 ? bottom : 0;
 							
-							var sidebarHeight = $el.outerHeight();
+					$el.css({bottom: bottom});
+				}
+
+				
+				if(previousScroll < currentScroll) {
+					if( !$el.hasClass('sticky-sidebar') ) {
 						
-							if( $(document).scrollTop() + $(window).outerHeight() >= sidebarHeight + $el.offset().top ) {
-								
-
-								$el.addClass('sticky-sidebar').css({ bottom: 0, width: $el.parent().width()});
-							}
-						}
-					} else {
-						if( $el.hasClass('sticky-sidebar') && $el.height() + $el.offset().top < $el.height() + $(opts.mainContainer).offset().top) {
+						var sidebarHeight = $el.outerHeight();
+					
+						if( $(document).scrollTop() + $(window).outerHeight() >= sidebarHeight + $el.offset().top ) {
 							
 
-							$el.removeClass('sticky-sidebar');
-							
-							
+							$el.addClass('sticky-sidebar').css({position: 'fixed', bottom: 0, width: $el.parent().width()});
 						}
+					}
+				} else {
+					if( $el.hasClass('sticky-sidebar') && $el.height() + $el.offset().top < $el.height() + $(opts.mainContainer).offset().top) {
+						
+
+						$el.removeClass('sticky-sidebar').css({position: 'relative'});
+						
+						
 					}
 				}
-				
+			}
+			
 
 
-				previousScroll = currentScroll;
-			})
+			previousScroll = currentScroll;
+		})
 
-		}
-		
-	})(jQuery);
+	}
+	
+})(jQuery);
